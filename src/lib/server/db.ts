f1d8f60024e7cb9f5593ec311478ff3618deae1a
alex16541 <aleks.16541@yandex.ru>
2024-12-28 14:20:00 +0300

    Edit: database env variable

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import { neon } from "@neondatabase/serverless";
 
-const connectionString = process.env.DATABASE_URL as string;
+const connectionString = process.env.POSTGRES_URL as string;
 
 export const sql = neon(connectionString);
